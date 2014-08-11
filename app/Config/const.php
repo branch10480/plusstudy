cdc71eaf26077f9aac3a818612b5f3cb5838de6c
toshicoo <toshiharu.imaeda@gmail.com>
2014-08-11 23:24:55 +0900

    const modified

--- app/Config/const.php
+++ app/Config/const.php
@@ -1,4 +1,4 @@
 <?php
-define('ROOT', 'http://' . env('HTTP_HOST') . '/plusstudy/');
-define('HOME', ROOT . '/home/top/');
+define('ROOT_URL', 'http://' . env('HTTP_HOST') . '/plusstudy/');
+define('HOME_URL', ROOT . '/home/top/');
 define('SITE_NAME', '+ Study');
