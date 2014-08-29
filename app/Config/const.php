d79508f724a155208c09123ab3c2a226e15c5b66
toshicoo <toshiharu.imaeda@gmail.com>
2014-08-29 19:22:09 +0900

    ハッシュに必要な定数を定義

--- app/Config/const.php
+++ app/Config/const.php
@@ -4,3 +4,5 @@ define('ROOT_PATH', env('HTTP_HOST') . '/plusstudy/');
 define('HOME_URL', ROOT_URL . '/home/top/');
 define('SITE_NAME', '+ Study');
 define('UP_PATH_SMN', '../webroot/img/seminar/');
+define('SALT_BLOWFISH', '$2a$11$inagakiimaedafukushima$');
+define('STRETCH_COUNT', 1000);
