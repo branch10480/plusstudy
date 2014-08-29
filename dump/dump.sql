88c4425227c8a6c5403c9cebe1b956ab507fc0f6
toshicoo <toshiharu.imaeda@gmail.com>
2014-08-30 01:57:19 +0900

    dump.sql を更新

--- dump/dump.sql
+++ dump/dump.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.13  Distrib 5.5.34, for osx10.6 (i386)
+-- MySQL dump 10.13  Distrib 5.5.29, for osx10.6 (i386)
 --
 -- Host: localhost    Database: plusstudy
 -- ------------------------------------------------------
--- Server version	5.5.34-log
+-- Server version	5.5.29
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -118,7 +118,7 @@ CREATE TABLE `me_toos` (
   `account_id` int(11) DEFAULT NULL,
   `resolved` char(1) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -128,6 +128,31 @@ CREATE TABLE `me_toos` (
 LOCK TABLES `me_toos` WRITE;
 /*!40000 ALTER TABLE `me_toos` DISABLE KEYS */;
 /*!40000 ALTER TABLE `me_toos` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `newacc_tmps`
+--
+
+DROP TABLE IF EXISTS `newacc_tmps`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `newacc_tmps` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `mailaddress` varchar(100) DEFAULT NULL,
+  `passwd` varchar(100) DEFAULT NULL,
+  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `newacc_tmps`
+--
+
+LOCK TABLES `newacc_tmps` WRITE;
+/*!40000 ALTER TABLE `newacc_tmps` DISABLE KEYS */;
+/*!40000 ALTER TABLE `newacc_tmps` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -198,7 +223,7 @@ CREATE TABLE `seminar_images` (
   `tmpid` int(11) DEFAULT '0',
   `size` int(11) DEFAULT '0',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -258,7 +283,7 @@ CREATE TABLE `teach_mes` (
   `title` varchar(100) DEFAULT NULL,
   `content` text,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -279,4 +304,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2014-08-27 19:17:55
+-- Dump completed on 2014-08-30  1:54:58
