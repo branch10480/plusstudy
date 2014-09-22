199235b90ab7ce520f996d419c5708792ce741b1
toshicoo <toshiharu.imaeda@gmail.com>
2014-09-22 19:37:35 +0900

    DBからのセミナー情報を取得->テキストボックスなどに反映、残り説明部分

--- app/Controller/SeminarsController.php
+++ app/Controller/SeminarsController.php
@@ -33,6 +33,231 @@ class SeminarsController extends AppController {
 
 
 /**
+ * edit method
+ *
+ * @return void
+ */
+	public function edit( $seminar_id = null ) {
+
+		if ($seminar_id === null) $this->redirect(array('controller' => 'Accounts', 'action' => 'index'));
+
+		$this->set('title_for_layout', '勉強会編集');
+
+
+		$fileUrlArr = array();
+		$smnName = '';
+		$place = '';
+		$upperLimit = 0;
+		$startDate = '';
+		$startH = 0;
+		$startM = 0;
+		$endH = 0;
+		$endM = 0;
+		$rsvLimitDate = '';
+		$rsvLimitH = 0;
+		$rsvLimitM = 0;
+		$dsc = '';
+		$smnImgId = '';
+
+		$eFileUrlArr = '';
+		$eSmnName = '';
+		$ePlace = '';
+		$eUpperLimit = '';
+		$eStartDate = '';
+		$eStartH = '';
+		$eStartM = '';
+		$eEndH = '';
+		$eEndM = '';
+		$eRsvLimitDate = '';
+		$eRsvLimitH = '';
+		$eRsvLimitM = '';
+		$eDsc = '';
+
+		$time = '';						// 一時的な時間変数
+
+
+
+
+
+		if ($this->referer() === ROOT_URL . 'Seminars/' . $this->action || $this->referer() === ROOT_URL . 'Seminars/' . $this->action . '/') {
+			// 自分自身から送信
+
+			// $fileUrlArr = array();
+			$validateResult = true;
+
+			$smnName = $this->request->data['Seminar']['name'];
+			$place = $this->request->data['Seminar']['place'];
+			$upperLimit = $this->request->data['Seminar']['upper_limit'];
+			$startDate = $this->request->data['Seminar']['date'];
+			$startH = $this->request->data['Seminar']['startH'];
+			$startM = $this->request->data['Seminar']['startM'];
+			$endH = $this->request->data['Seminar']['endH'];
+			$endM = $this->request->data['Seminar']['endM'];
+			$rsvLimitDate = $this->request->data['Seminar']['reservation_limit_d'];
+			$rsvLimitH = 0;
+			$rsvLimitM = 0;
+			$dsc = $this->request->data['Seminar']['description'];
+			$smnImgId = $this->request->data['Seminar']['seminar_img_id'];
+
+			// --- バリデーションチェック ---
+			// 勉強会名
+			if ($smnName === '') {
+				$eSmnName = '何も入力されていません';
+				$validateResult = false;
+			} else if (!preg_match('/.{1,20}/', $smnName)) {
+				$eSmnName = '入力された文字列が長すぎます';
+				$validateResult = false;
+			}
+
+			// 開催場所
+			if ($place === '') {
+				$ePlace = '何も入力されていません';
+				$validateResult = false;
+			} else if (!preg_match('/.{1,50}/', $place)) {
+				$ePlace = '入力された文字列が長すぎます';
+				$validateResult = false;
+			}
+
+			// 参加人数上限
+			if ($upperLimit === '') {
+				$eUpperLimit = '何も入力されていません';
+				$validateResult = false;
+			} else if (!preg_match('/^(0|[1-9][0-9]*)$/', $upperLimit)) {
+				$eUpperLimit = '半角数字で入力してください';
+				$validateResult = false;
+			}
+
+			// 開催日
+			if ($startDate === '') {
+				$eStartDate = '何も入力されていません';
+				$validateResult = false;
+			} else if (!preg_match('/^[1,2][0-9]{3}-(01|02|03|04|05|06|07|08|09|10|11|12)-(01|02|03|04|05|06|07|08|09|10|11|12|13|14|15|16|17|18|19|20|21|22|23|24|25|26|27|28|29|30|31)$/', $startDate)) {
+				$eStartDate = '半角数字で0000-00-00の書式で入力してください';
+				$validateResult = false;
+			}
+
+			// 予約締め切り日
+			if ($rsvLimitDate === '') {
+				$eRsvLimitDate = '何も入力されていません';
+				$validateResult = false;
+			} else if (!preg_match('/^[1,2][0-9]{3}-(01|02|03|04|05|06|07|08|09|10|11|12)-(01|02|03|04|05|06|07|08|09|10|11|12|13|14|15|16|17|18|19|20|21|22|23|24|25|26|27|28|29|30|31)$/', $rsvLimitDate)) {
+				$eRsvLimitDate = '半角数字で0000-00-00の書式で入力してください';
+				$validateResult = false;
+			}
+
+
+			if ($validateResult) {
+
+				// 受け渡し用Session作成
+				$this->Session->write('newSmn', $this->request->data);
+
+				// 確認ページへ
+				$this->redirect(array('action' => 'newSmnConfirm'));
+			}
+		} else {
+			//----- 登録済みの勉強会情報を取得 -----
+			$param = array(
+					'conditions' => array(
+							'Seminar.id' => $seminar_id,
+						),
+				);
+
+			$result = '';
+			if (!$result = $this->Seminar->find('first', $param)) $this->redirect(array('controller' => 'Accounts', 'action' => 'index'));
+			var_dump($result);
+
+
+
+			//----- datetime型の値を分割処理 -----
+			list($rsvLimitDate, $time) = split(" ", $result['Seminar']['reservation_limit']);
+			list($rsvLimitH, $rsvLimitM) = split(":", $time);
+			$this->request->data['Seminar']['reservation_limit_d'] = $rsvLimitDate;
+			$this->request->data['Seminar']['reservation_limit_h'] = $rsvLimitH;
+			$this->request->data['Seminar']['reservation_limit_m'] = $rsvLimitM;
+
+			list($startDate, $time) = split(" ", $result['Seminar']['start']);
+			list($startH, $startM) = split(":", $time);
+			$this->request->data['Seminar']['date'] = $startDate;
+			$this->request->data['Seminar']['startH'] = $startH;
+			$this->request->data['Seminar']['startM'] = $startM;
+
+			// その他のパラメータ格納
+			$this->request->data['Seminar']['name'] = $result['Seminar']['name'];
+			$this->request->data['Seminar']['place'] = $result['Seminar']['place'];
+			$this->request->data['Seminar']['upper_limit'] = $result['Seminar']['upper_limit'];
+			$this->request->data['Seminar']['place'];
+			$this->request->data['Seminar']['description'] = $result['Seminar']['description'];
+
+
+
+
+		}
+
+
+
+		// 詳細から戻ってきたときの処理
+		// if ($this->referer() === ROOT_URL . 'Seminars/newSmnConfirm' || $this->referer() === ROOT_URL . 'Seminars/newSmnConfirm/') {
+
+		// 	if ($this->Session->check('newSmn')) {
+		// 		$this->request->data = $this->Session->read('newSmn');
+		// 		$dsc = $this->Session->read('newSmn')['Seminar']['description'];
+		// 		$smnImgId = $this->Session->read('newSmn')['Seminar']['seminar_img_id'];
+		// 	}
+		// }
+
+
+		$minArray = array();
+		for ($i=0; $i<60; $i+=5) {
+			$minArray[] = $i;
+		}
+
+		$hArray = array();
+		for ($i=0; $i<24; $i++) {
+			$hArray[] = $i;
+		}
+
+		$fontsizeArray = array();
+		for ($i=0; $i<10; $i++) {
+			$fontsizeArray[] = $i;
+		}
+
+		$fontColor = array(
+				'black' => '黒',
+				'red' => '赤',
+				'orange' => 'オレンジ',
+				'blue' => '青',
+				'green' => '緑',
+			);
+
+		$this->set(array(
+				'minArray' => $minArray,
+				'hArray' => $hArray,
+				'fontsizeArray' => $fontsizeArray,
+				'fontColor' => $fontColor,
+				'eSmnName' => $eSmnName,
+				'ePlace' => $ePlace,
+				'eUpperLimit' => $eUpperLimit,
+				'eStartDate' => $eStartDate,
+				'eRsvLimitDate' => $eRsvLimitDate,
+				// 'smnName' => $smnName,
+				// 'place' => $place,
+				'upperLimit' => $upperLimit,
+				// 'startDate' => $startDate,
+				// 'startH' => $startH,
+				// 'startM' => $startM,
+				// 'endH' => $endH,
+				// 'endM' => $endM,
+				// 'rsvLimitDate' => $rsvLimitDate,
+				'dsc' => $dsc,
+				'smnImgId' => $smnImgId,
+				'accId' => $this->Session->read('Auth.id'),
+			));
+	}
+
+
+
+
+/**
  * index method
  *
  * @return void
---
+++ app/webroot/js/SeminarsEdit.js
@@ -0,0 +1,222 @@
+// SeminarsEdit.js
+
+//*****************************************************************************
+//
+// main
+//
+//*****************************************************************************
+$(function () {
+
+	//----- セミナー画像一覧取得 -----
+	getSmnImgs();
+
+	//----- モーダルウィンドウ設定処理 -----
+	$.setModalWin('#selectImgsBtn', '#insertImg');
+
+	document.getElementById('selectImgsBtn').addEventListener("click", function () {
+		window.selectImgType = 0;
+	});
+	document.getElementById('insertImg').addEventListener("click", function () {
+		window.selectImgType = 1;
+	});
+
+	//----- 画像アップロード -----
+	$('#imgUpForm').submit(function(event) {
+		event.preventDefault();
+
+		// 画像が選択されているかチェック
+		if ($('#imgFile').val() === '') {
+			alert('画像が選択されていません');
+			return;
+		}
+
+		var callbacks_ = {
+				'begin'   : function(){},
+				'success' : function(data){
+					if (data[0] ==='true') {
+						alert('画像アップロード完了！');
+						getSmnImgs();
+					} else {
+						alert(data[1]);
+					}
+				},
+				'error'   : function(){},
+				'complete': function(){},
+		};
+		ajax_submit($(this), callbacks_);
+	});
+
+	//----- セミナー画像リセットボタン -----
+	$('#smnImgReset').click((function(){
+		var coverImg = $('#coverImg');
+		var smnImgId = $('#SeminarSeminarImgId');
+		return function(event){
+			event.preventDefault();
+			coverImg.html('');
+			smnImgId.val('');
+		};
+	})());
+
+
+	//----- ページング処理登録 -----
+	window.disp = (function(){
+
+		//--- メンバ ---
+		var page = 1;
+		var dispNo = 2;					// 1ページに表示する数
+		var data = null;
+		var maxPage = 1;
+		var myImgs = null;
+		var pagingInfo = null;
+		var storage = null;
+
+		//--- 処理 ---
+		return function ( dataArr, preOrNxt ) {
+
+			if (arguments[0] !== null) data = dataArr;
+			// 前のページ、もしくは次のページに移る場合の処理
+			if (arguments[1]) {
+				if (preOrNxt === 'pre') {
+					// 前ページに進む処理
+					if (page-1 < 1)
+						page = maxPage;
+					else
+						page--;
+				} else if (preOrNxt === 'nxt') {
+					// 次ページに進む処理
+					if (page+1 > maxPage)
+						page = 1;
+					else
+						page++;
+				}
+			}
+			maxPage = Math.floor((data.length - 1)/dispNo) + 1;
+
+			// 要素をキャッシュ
+			if (myImgs === null) myImgs = $('#myImgs');
+			if (pagingInfo === null) pagingInfo = $('#pagingNav .info');
+			if (storage === null) storage = $('#storage');
+
+			var dispCnt = 0;
+			var dataNo = 0;					// 表示する配列の添え字
+
+			// モーダルウィンドウ内出力データ整形
+			var outStr = '';
+			for (dataNo = (page-1)*dispNo; dataNo<data.length; dataNo++) {
+				dispCnt++;
+				if (dispCnt > dispNo) break;
+				outStr += '<li><a class="delBtn" onclick="delSmnImg(event)" href="#">×</a><img class="smnImg" onload="optim();" onclick="selectImg(event)" src="' + WEB_ROOT + 'img/seminar/' + data[dataNo]['SeminarImage']['id'] + data[dataNo]['SeminarImage']['ext'] + '" alt="' + data[dataNo]['SeminarImage']['description'] + '" width="' + data[dataNo]['SeminarImage']['width'] + '" height="' + data[dataNo]['SeminarImage']['height'] + '" /><input type="hidden" value="' + data[dataNo]['SeminarImage']['id'] + data[dataNo]['SeminarImage']['ext'] + '" /></li>';
+			}
+
+			// 画像出力
+			myImgs.html(outStr);
+
+			// ページング情報部更新
+			pagingInfo.html(page + ' / ' + maxPage);
+
+			// 使用容量更新
+			var mydatasize = 0;
+			for (var i=0; i<data.length; i++) {
+				mydatasize = mydatasize + +data[i]['SeminarImage']['size'];
+			}
+			storage.html(sprintf('%.3f', mydatasize/(1024*1024)) + 'MB / 50MB');
+
+		};
+	})();
+	$('#pagingNav li .pre').click(function(event) {
+		event.preventDefault();
+		disp(null, 'pre');
+	});
+	$('#pagingNav li .nxt').click(function(event) {
+		event.preventDefault();
+		disp(null, 'nxt');
+	});
+
+});
+
+
+
+//*****************************************************************************
+//
+// Declaration Area
+//
+//*****************************************************************************
+function selectImg(event) {
+	switch (window.selectImgType) {
+		case 0:
+			// カバー画像を選択
+			var coverImgDispArea = document.getElementById('coverImg');
+			var coverImg = document.createElement('img');
+			coverImg.src = event.target.src;
+			// coverImgDispArea.removeChild(coverImgDispArea.childNodes[0]);
+			// console.log(coverImgDispArea.childNodes);
+			// coverImgDispArea.appendChild(coverImg);
+			$('#coverImg').html('').append(coverImg);
+			$('#SeminarSeminarImgId').val(event.target.src);
+			$.closeModalWin();
+			break;
+
+		case 1:
+			// リッチエディタに画像挿入処理
+			var newImg = document.createElement('img');
+			newImg.src = event.target.src;
+			document.getElementById('editArea').appendChild(newImg);
+			$.closeModalWin();
+			break;
+
+		default:
+			break;
+	}
+}
+
+function getSmnImgs() {
+	$.ajax({
+		url: WEB_ROOT + 'SeminarImages/getSmnImgs/' + $('#accId').val(),
+		type: 'POST',
+		dataType: 'json',
+	})
+	.done(function(data) {
+		console.log(data);
+
+		// 出力
+		disp( data );
+
+	})
+	.fail(function() {
+		console.log("error");
+	})
+	.always(function() {
+		console.log("complete");
+	});
+}
+
+function optim() {
+	ImgOpt.setImgId('.smnImg');
+	ImgOpt.optimize();
+}
+
+function delSmnImg(event) {
+	// alert($(event.target).parent().find('input').val());
+	if (!window.confirm('この写真を削除します。よろしいですか？')) return;
+
+	//----- 削除処理 -----
+	$.ajax({
+		url: WEB_ROOT + 'SeminarImages/delSmnImgs/' + $(event.target).parent().find('input').val(),
+		type: 'POST',
+		dataType: 'json',
+	})
+	.done(function(data) {
+		console.log(data);
+
+		// 画像データの再取得&表示
+		getSmnImgs();
+
+	})
+	.fail(function() {
+		console.log("error");
+	})
+	.always(function() {
+		console.log("complete");
+	});
+
+}
