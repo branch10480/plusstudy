30a5506137ccba67c81b03fa7ac138d5a1c83b31
User Plusstudy <plusstudy@www2777.sakura.ne.jp>
2015-03-05 13:12:47 +0900

    const.php modified

--- app/Config/const.php
+++ app/Config/const.php
@@ -1,6 +1,6 @@
 <?php
-define('ROOT_URL', 'http://' . env('HTTP_HOST') . '/plusstudy/');
-define('ROOT_PATH', env('HTTP_HOST') . '/plusstudy/');
+define('ROOT_URL', 'http://' . env('HTTP_HOST') . '/');
+define('ROOT_PATH', env('HTTP_HOST') . '/');
 define('HOME_URL', ROOT_URL . '/home/top/');
 define('SITE_NAME', 'Plus Study');
 define('UP_PATH_SMN', '../webroot/img/seminar/');
